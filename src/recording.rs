042fc8475838ed6cfb75812ffe20186770afda2f
Siddharth Atre <sid.atre@me.com>
2021-01-09 19:40:38 -0800

    More work on recording.

--- src/recording.rs
+++ src/recording.rs
@@ -1,3 +1,4 @@
+use crate::{utils, vector::IntVector2};
 use ffmpeg::{encoder, rational::Rational, util::color::Space, util::format::Pixel};
 use ffmpeg_next as ffmpeg;
 use std::time::Instant;
@@ -10,17 +11,18 @@ pub struct Recorder {
     encoder: encoder::video::Video,
     frame_count: usize,
     start_time: Instant,
-    width: usize,
-    height: usize,
+    width: u32,
+    height: u32,
+    pixel_format: Pixel,
 }
 
 impl Recorder {
-    pub fn new(width: usize, height: usize, format: Pixel) -> Recorder {
+    pub fn new(width: u32, height: u32, format: Pixel) -> Recorder {
         let mut encoder = ffmpeg::codec::encoder::new();
         let mut video_encoder = encoder.video().unwrap();
         video_encoder.set_format(format);
-        video_encoder.set_width(width as u32);
-        video_encoder.set_height(height as u32);
+        video_encoder.set_width(width);
+        video_encoder.set_height(height);
         // We assume some features of the encoder for now.
         video_encoder.set_colorspace(Space::RGB);
         video_encoder.set_frame_rate(Some(Rational::new(1, 60)));
@@ -32,6 +34,14 @@ impl Recorder {
             start_time: Instant::now(),
             width,
             height,
+            pixel_format: format,
         }
+    }
+
+    async fn encode_frame(&mut self, buffer: wgpu::Buffer, resolution: IntVector2) {
+        // Create an FFmpeg frame.
+        let mut frame = ffmpeg::util::frame::Video::new(self.pixel_format, self.width, self.height);
+        let pixel_data = utils::transcode_painting_data(buffer, resolution).await;
+        let frame_data = frame.data_mut(0);
     }
 }
