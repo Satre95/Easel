f636d6e985025188929d504ba57c7aa1fa46668b
Siddharth Atre <sid.atre@me.com>
2021-05-01 20:55:26 -0700

    Update skeleton.

--- src/skeletons.rs
+++ src/skeletons.rs
@@ -1,31 +1,35 @@
 /// Source string of the skeleton of a typical Easel shader.
 pub static SHADER_SKELETON: &str = r#"
 #version 450
-#ifndef MY_SHADER_FRAG
-#define MY_SHADER_FRAG
 
 layout(set = 0, binding = 0) uniform Uniforms {
     vec4 u_resolution;
+    vec4 u_mouse_info;
+    ivec4 u_mouse_button_pressed;
+    ivec4 u_date;
     float u_time;
     float u_time_delta;
     uint u_frame_num;
-    vec4 u_mouse_info;
+    uint u_num_textures;
 };
 
+layout(set = 0, binding = 1) uniform CustomUniforms { bool vertical_wipe; };
+
 layout(location = 0) in vec2 tex_coords;
-layout(location = 0) out vec4 output_color;
+layout(location = 0) out vec4 f_color;
 
 void main() {
-    float window_wiper = sin(0.5f * u_time);
+    float window_wiper = sin(0.5 * u_time);
     window_wiper *= window_wiper;
 
-    if (tex_coords.x < window_wiper) {
-        output_color = vec4(0.f, 0.5f, 0.5f, 1.f);
+    bool color_condition = (vertical_wipe) ? tex_coords.y < window_wiper
+                                           : tex_coords.x < window_wiper;
+
+    if (color_condition) {
+        f_color = vec4(0.f, 0.5f, 0.5f, 1.f);
     } else {
-        output_color = vec4(0.5f, 0.f, 1.f, 1.f);
+        f_color = vec4(0.5f, 0.f, 1.f, 1.f);
     }
 }
-
-#endif
 
 "#;
